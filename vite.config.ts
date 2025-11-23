b48e39e8e507904584a8fe21640303df08d563e0
hassanrizvi30-cloud <hassanrizvi30@gmail.com>
2025-11-23 06:34:19 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: "/MSQ-ENTERPRISE/",   // ⭐ GITHUB PAGES FIX — MUST HAVE
       server: {
         port: 3000,
         host: '0.0.0.0',
